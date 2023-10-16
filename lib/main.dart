6d63d7d142efd69154d17e953ffe8e583881c5e4
= <=>
2023-10-16 08:52:58 +0500

    my headhurts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
@@ -14,10 +14,9 @@ bool isFirebaseReady = true;
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-          // options: DefaultFirebaseOptions.currentPlatform,
-          )
-      .catchError((e) {
-    isFirebaseReady = false;
+    options: DefaultFirebaseOptions.currentPlatform,
+  ).catchError((e) {
+   
     print(e);
   });
   prefs = await SharedPreferences.getInstance();
